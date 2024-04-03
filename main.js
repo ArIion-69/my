8ea689d8119673ac8c2ba684f1005abdea7d67c1
lachkov danil <lachkow087@gmail.com>
2024-04-03 21:14:41 +0500

    init

---
+++ main.js
@@ -0,0 +1,27 @@
+     
+      /* Анимация 3D моего фото */
+      
+      
+      let elem = document.getElementById('card');
+      let rect = elem.getBoundingClientRect();
+      
+      function transformation(event) {
+          let x = event.clientX;
+          let y = event.clientY;
+          console.log(x,y,rect.x,rect.y);
+          makeTransformation(x, y)
+      }
+      
+      function makeTransformation(x, y) {
+          let x1 = x - (rect.x + (rect.width / 2));
+          let y1 = y - (rect.y + (rect.height / 2));
+          
+          console.log(x1, y1);
+          console.log(Math.cos(x1) * 20, Math.cos(y1) * 20);
+          
+          elem.style.transform = `translateZ(10px) rotateX(${x1/5}deg) rotateY(${y1/8}deg)`;
+      }
+      
+      function stablecard() {
+          elem.style.transform = `translateZ(0px) rotateX(0deg) rotateY(0deg)`    ;
+      }
